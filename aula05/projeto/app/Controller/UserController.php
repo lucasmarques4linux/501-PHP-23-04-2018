60ed415e1de7114f4082c834bf2f92bae9fb66e9
lucasmarques73@hotmail.com <lucasmarques73@hotmail.com>
2018-04-27 17:43:31 -0300

    projeto init login

---
+++ aula05/projeto/app/Controller/UserController.php
@@ -0,0 +1,53 @@
+<?php 
+
+namespace Controller;
+
+use Lib\ViewModel\ViewModel;
+use Model\UserModel;
+
+class UserController
+{
+	private $viewModel;
+	private $userModel;
+
+	public function __construct()
+	{
+		$this->viewModel = new ViewModel();
+		$this->userModel = new UserModel();
+	}
+
+	public function index()
+	{
+		$users = $this->userModel->all();
+		$this->viewModel->render('user/index',['users' => $users]);
+	}
+
+	public function new()
+	{
+		$this->viewModel->render('user/new');
+	}
+
+	public function edit($id)
+	{	
+		$user = $this->userModel->findOne($id);
+		$this->viewModel->render('user/edit',['user'=>$user]);
+	}
+
+	public function create()
+	{
+		$this->userModel->create($_POST);
+		header('location:?r=user');
+	}
+
+	public function update()
+	{
+		$this->userModel->edit($_POST);
+		header('location:?r=user');
+	}
+
+	public function delete()
+	{
+		$this->userModel->delete($_POST['id']);
+		header('location:?r=user');	
+	}
+}
---
+++ aula05/projeto/app/View/layout/header.php
@@ -0,0 +1,7 @@
+<head>
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+</head>
+<body>
+<div style="margin: 25px 100px 25px 100px">
+<h1>MVC5 - FrontController</h1>
+<hr>
